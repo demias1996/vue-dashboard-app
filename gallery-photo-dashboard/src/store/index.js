58356237876f0eb02b735ddf35d2fab5c6743fe8
Aleksandr Derkach <demias1996@gmail.com>
2020-12-30 22:17:24 +0200

    add axios, start to create api model

--- gallery-photo-dashboard/src/store/index.js
+++ gallery-photo-dashboard/src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import actions from './actions'
+import mutations from './mutations'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: [],
+    albums: [],
+    photos: []
   },
-  mutations: {
-  },
-  actions: {
-  },
+  mutations,
+  actions,
   modules: {
   }
 })
---
+++ gallery-photo-dashboard/src/store/mutations.js
@@ -0,0 +1,11 @@
+export default {
+    SET_USERS(state, users){
+        state.users = users;
+    },
+    SET_PHOTOS(state, photos){
+        state.photos = photos;
+    },
+    SET_ALBUMS(state, albums){
+        state.albums = albums;
+    }
+}
