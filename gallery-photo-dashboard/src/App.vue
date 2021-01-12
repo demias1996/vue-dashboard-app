66e132bd4d486f731af0dd5bbb5ab0126b4b9f16
Aleksandr Derkach <demias1996@gmail.com>
2021-01-12 23:47:31 +0200

    add users list block to app and create userListItem component

--- gallery-photo-dashboard/src/App.vue
+++ gallery-photo-dashboard/src/App.vue
@@ -1,50 +1,123 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div class="sidebar">
+      <div class="logoWrapper">
+        <img src="@/assets/linux.svg" class="logo" />
+      </div>
+      <h3 class="title">Subscriptions</h3>
+      <div class="subscriptionsWrapper">
+          <ul>
+            <UsersListItem
+              v-for="user in users"
+              :key="user.id"
+              :userItemData="user"
+            />
+          </ul>
+      </div>
+    </div>
+    <div class="wrapper">
+      <div class="searchWidget">
+        <div class="searchWidget__input"></div>
+        <div class="searchWidget__userSelector"></div>
+      </div>
+    </div>
+
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
 
 <script>
+
+  import UsersListItem from './components/UsersListItem'
+
 export default {
-  data(){
+  components: {
+   UsersListItem 
+  },
+  data() {
     return {
-      test: "test"
+      test: "test",
     };
   },
-  computed:{
+  computed: {
+    users(){
+      return this.$store.state.users;
+    }
 
   },
-  beforeMount(){
-   this.$store.dispatch('GET_USERS');
-   this.$store.dispatch('GET_ALBUMS');
-   this.$store.dispatch('GET_PHOTOS');
-  }
-}
+  beforeMount() {
+    this.$store.dispatch("GET_USERS");
+    this.$store.dispatch("GET_ALBUMS");
+    this.$store.dispatch("GET_PHOTOS");
+  },
+};
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: grid;
+  margin: 0;
+  padding: 0;
+  grid-template-columns: 1fr 4fr;
+  grid-template-rows: 100%;
+  grid-gap: 0;
+  height: 100vh;
 }
 
-#nav {
-  padding: 30px;
+.sidebar {
+  background-color: blue;
+  padding-top: 10%;
+  padding-bottom: 10%;
+  display: grid;
+  grid-gap: 1%;
+  grid-template-rows: 3fr 1fr 17fr;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.wrapper {
+  background-color: yellow;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.logoWrapper {
+  width: 100%;
+  text-align: center;
+}
+
+.logo {
+  max-width: 8vw;
+  height: auto;
+}
+
+ .title {
+    text-align: left;
+    padding-right: 7%;
+    padding-left: 7%;
   }
+
+.subscriptionsWrapper {
+  text-align: left;
+  overflow: auto;
+
+  ul {
+    margin:0;
+    padding-right: 7%;
+    padding-left: 7%;
+  }
+  
+
+
 }
 </style>
---
+++ gallery-photo-dashboard/src/components/UsersListItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <li class="listItem">
+        <div class="userLogoWrapper">
+            <img src="@/assets/google.svg" class="userLogo" alt="user_logo">
+        </div>
+        <div class="userNameWrapper">
+            <p class="userName">
+                {{userItemData.name}}
+            </p>
+        </div>
+    </li>
+</template>
+<script>
+export default {
+    name: "UsersListItem",
+    props: {
+        userItemData: Object
+    },
+    
+}
+</script>
+<style lang="scss" scoped>
+    .listItem {
+        padding-top: 5px;
+        padding-bottom: 5px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        height: 30px;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        .userLogoWrapper{
+            height: 100%;
+            padding: 5%;
+            text-align: center;
+            vertical-align: center;
+
+            .userLogo {
+                height: 100%;
+                width: auto;
+            }
+        }
+    }
+
+</style>
